6dc8c05352822142e8d6fd1758fce994451fdf14
baurchanu <baurchanu.k@gmail.com>
2017-10-20 10:09:17 +0400

    HW04.1 CacheInjector: The method inject() has been refactored (among other things the invocation of the method updateMapOfCaches() has been removed). The method getMapOfCaches() has been renamed as updateMapOfCaches() and rewritten,its access modifier has been changed to public. The method getAllFields() has been refactored. Executor: An invocation of the method updateMapOfCaches() has been added to the method execute().

--- src/main/java/hw04/reflection/CacheInjector.java
+++ src/main/java/hw04/reflection/CacheInjector.java
@@ -6,86 +6,64 @@ import hw04.reflection.cache.Cache;
 import hw04.reflection.cluster.ClusterOfCaches;
 
 import java.io.File;
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 import java.util.*;
 
 public class CacheInjector {
-    public static void inject(ClusterOfCaches cluster, String scannedPackage) {
-        Map<String, Class<?>> mapOfCaches = getMapOfCaches(scannedPackage);
+    private static Map<String, Class<?>> mapOfCaches = new HashMap<>();
 
+    public static void inject(ClusterOfCaches cluster) {
         List<Field> fields = new ArrayList<>();
         getAllFields(fields, cluster.getClass());
 
         System.out.println("Class:");
         System.out.println(cluster.getClass() + "\n");
-
         for (Field f : fields) {
             System.out.println("Field:");
             System.out.println(f);
-            boolean accessChanged = false;
-            Annotation[] annotations = f.getDeclaredAnnotations();
-            if (annotations.length != 0) {
-                for (Annotation ann : annotations) {
-                    if (ann.annotationType().getTypeName().equals(InjectCache.class.getTypeName())) {
-                        InjectCache injectCache = (InjectCache) ann;
-                        if (!f.canAccess(cluster)) {
-                            f.setAccessible(true);
-                            accessChanged = true;
-                        }
-                        Cache cache = null;
-                        try {
-                            cache = (Cache) mapOfCaches.get(injectCache.name()).getConstructor().newInstance();
-                        } catch (InstantiationException | IllegalAccessException
-                                | InvocationTargetException | NoSuchMethodException e) {
-                            System.out.printf("The exception has been thrown while trying to create new instance "
-                                    + "of the cache with Reflection API!");
-                        }
-
-                        try {
-                            f.set(cluster, cache);
-                        } catch (IllegalAccessException e) {
-                            System.out.println("IllegalAccessException has been thrown while trying to inject the field!");
-                        }
-                        if (accessChanged) {
-                            f.setAccessible(false);
-                        }
-                        break;
-                    }
+            InjectCache injectCache = f.getAnnotation(InjectCache.class);
+            if (injectCache != null) {
+                f.setAccessible(true);
+                Cache cache = null;
+                try {
+                    cache = (Cache) mapOfCaches.get(injectCache.name()).getConstructor().newInstance();
+                } catch (InstantiationException | IllegalAccessException
+                        | InvocationTargetException | NoSuchMethodException e) {
+                    System.out.printf("The exception has been thrown while trying to create new instance "
+                            + "of the cache with Reflection API!");
+                }
+                try {
+                    f.set(cluster, cache);
+                } catch (IllegalAccessException e) {
+                    System.out.println("IllegalAccessException has been thrown while trying to inject the field!");
                 }
             } else {
-                System.out.println("Annotations not found!");
+                System.out.println("The annotation is not found.");
             }
             System.out.println();
         }
     }
 
-    private static Map<String, Class<?>> getMapOfCaches(String scannedPackage) {
+    public static void updateMapOfCaches(String scannedPackage) {
         Map<String, Class<?>> mapOfCaches = new HashMap<>();
         List<Class<?>> cacheClasses = ClassFinder.find(scannedPackage);
-        if (cacheClasses == null) {
-            return mapOfCaches;
-        }
         for (Class c : cacheClasses) {
-            Annotation[] annotations = c.getAnnotations();
-            if (annotations.length != 0) {
-                for (Annotation a : annotations) {
-                    if (a.annotationType().getTypeName().equals(CacheDeclaration.class.getTypeName())) {
-                        CacheDeclaration cd = (CacheDeclaration) a;
-                        mapOfCaches.put(cd.name(), c);
-                    }
-                }
+            CacheDeclaration cd = (CacheDeclaration) c.getAnnotation(CacheDeclaration.class);
+            if (cd != null) {
+                mapOfCaches.put(cd.name(), c);
             }
         }
-        return mapOfCaches;
+        CacheInjector.mapOfCaches = mapOfCaches;
     }
 
     private static void getAllFields(List<Field> fields, Class<?> c) {
         fields.addAll(Arrays.asList(c.getDeclaredFields()));
-        if (c.getSuperclass() != null) {
-            getAllFields(fields, c.getSuperclass());
+        if (!c.getCanonicalName().equals(ClusterOfCaches.class.getCanonicalName())) {
+            if (c.getSuperclass() != null) {
+                getAllFields(fields, c.getSuperclass());
+            }
         }
     }
 
--- src/main/java/hw04/reflection/Executor.java
+++ src/main/java/hw04/reflection/Executor.java
@@ -5,7 +5,8 @@ import hw04.reflection.cluster.ClusterOfFourCaches;
 public class Executor {
     public static void execute() {
         ClusterOfFourCaches cluster = new ClusterOfFourCaches();
-        CacheInjector.inject(cluster, "hw04.reflection.cache");
+        CacheInjector.updateMapOfCaches("hw04.reflection.cache");
+        CacheInjector.inject(cluster);
         ClusterCustomer customer = new ClusterCustomer(cluster);
         customer.putTestValues();
         customer.getTestValues();
